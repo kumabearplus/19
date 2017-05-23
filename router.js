51d96e7fb8d502316dd0a04129bb5339f814cdaa
kumabearpluss <1229425590@qq.com>
2017-05-23 17:34:35 +0800

    999

---
+++ router.js
@@ -0,0 +1,40 @@
+app.get('/loadMore',function(req,res) {
+  var index = req.query.index
+  var length = req.query.length
+  var data = []
+  for (var i = 0; i < length; i++) {
+  	data.push('内容' + (parseInt(index) + i+1))
+  }
+  setTimeout(send,1000)
+  function send(){
+  	res.send({
+      status:0,
+      data:data
+    })
+  }
+  // res.send({
+  //     status: 0,
+  //     data: data
+  //   })
+
+
+	// body...
+})
+
+app.post('/loadMore',function(req,res) {
+  var index = req.body.index
+  var length = req.body.length
+  var data = []
+  for (var i = 0; i < length; i++) {
+  	data.push('内容' + (parseInt(index) + i+1))
+  }
+  setTimeout(send,1000)
+  function send(){
+  	res.send({
+      status:0,
+      data:data
+    })
+  }
+  
+	// body...
+})
